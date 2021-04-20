d30c5b411a3e732095b89782abf655bb0d147e88
unknown <jahnavisananse@gmail.com>
2021-04-20 19:46:35 +0530

    api calling using fetch & axios

---
+++ fetchDemo.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+
+const fetchDemo = () => {
+  const [data, setData] = useState('');
+  const [list, setList] = useState('');
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/jahnavisananse`)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => {
+        alert(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos/1`)
+      .then(response => setList(response.data))
+      .catch(error => {
+        alert(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <View style={{flexDirection: 'column', backgroundColor: 'yellow'}}>
+        <Text style={styles.textFont}> using fetch</Text>
+        <Text style={styles.textFont}> ID : {data.id}</Text>
+        <Text style={styles.textFont}> NAME : {data.name}</Text>
+        <Text style={styles.textFont}> LOGIN : {data.login}</Text>
+        <Text style={styles.textFont}> FOLLOWERS : {data.followers}</Text>
+        <Text style={styles.textFont}> FOLLOWING : {data.following}</Text>
+      </View>
+      <View style={{flexDirection: 'column', backgroundColor: 'pink'}}>
+        <Text style={styles.textFont}> using axios</Text>
+        <Text style={styles.textFont}> List : {list.userId}</Text>
+        <Text style={styles.textFont}> List : {list.id}</Text>
+        <Text style={styles.textFont}> List : {list.title}</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: '90%',
+  },
+  textFont: {fontSize: 20},
+});
+
+export default fetchDemo;
